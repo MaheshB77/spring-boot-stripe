beff23ed0dcebd7311125557a98a5a1ffdddc528
MaheshB7 <mbansode7696@gmail.com>
2022-04-17 15:23:26 +0530

    Initial commit

---
+++ src/test/java/com/mahesh/springbootstripe/SpringBootStripeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mahesh.springbootstripe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStripeApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
